dd3b74c24ac54618263919e70facaf5b746d9043
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-09-17 09:04:32 +0800

    Git Hook with Husky
    Changes to be committed:
    	new file:   .husky/pre-commit
    	modified:   app/package.json
    	modified:   app/tests/unit/example.spec.js
    	modified:   app/yarn.lock
    
    1) Include initial example of hook
       by verifying the unit test passed in pre-commit interruptions,
       before commit.
    
    2) .husky file must be in the same directory of .git file.

--- app/tests/unit/example.spec.js
+++ app/tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Screen from '@/components/Screen.vue';
 
-describe('HelloWorld.vue', () => {
+describe('Screen.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Screen, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
