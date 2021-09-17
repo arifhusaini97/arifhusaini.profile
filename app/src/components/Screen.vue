c880f61cada21fa7af781d7f99f1711abddd49f4
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-09-17 22:03:28 +0800

    Basic Layout (Major Adjustment)
    Changes to be committed:
    	modified:   app/src/App.vue
    	deleted:    app/src/assets/logo.png
    	new file:   app/src/assets/system_logo.png
    	modified:   app/src/components/Screen.vue
    	modified:   app/src/sass/base/_base.scss
    	new file:   app/src/sass/components/_legal.scss
    	modified:   app/src/sass/components/_navigation.scss
    	modified:   app/src/sass/components/_screen.scss
    	modified:   app/src/sass/main.scss

--- app/src/components/Screen.vue
+++ app/src/components/Screen.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="screen">
     <div class="screen__sidebar">
-      <div class="screen__sidebar-navigation">
-        {{ msg }}
+      <div class="navigation">
+        <div class="navigation-logo"></div>
+        <ul class="navigation-list">
+          <li class="navigation-list__item navigation-list__item--active">
+            <a href="#" class="navigation-list__item-link">Global</a>
+          </li>
+          <li class="navigation-list__item">
+            <a href="#" class="navigation-list__item-link">List</a>
+          </li>
+          <li class="navigation-list__item">
+            <a href="#" class="navigation-list__item-link">Form</a>
+          </li>
+        </ul>
       </div>
-      <div class="screen__sidebar-legal">
-        &copy; 2021 by arifhusaini.com. All rights reserved.
+      <div class="legal">
+        &copy; 2021 by arifhusaini.com.<br />All rights reserved.
       </div>
     </div>
     <div class="screen__header">
