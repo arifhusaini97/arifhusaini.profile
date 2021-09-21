89d6082e9aa62dec57c30bd16879c9d1342c6c99
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-09-21 08:24:11 +0800

    Floating Compose Menu (Basic Search)
    Changes to be committed:
    	modified:   app/src/components/sub-components/FloatingComposeButton.vue
    	modified:   app/src/sass/components/_button.scss
    	new file:   app/src/sass/components/_form.scss
    	new file:   app/src/sass/components/_icon.scss
    	modified:   app/src/sass/main.scss

--- app/src/components/sub-components/FloatingComposeButton.vue
+++ app/src/components/sub-components/FloatingComposeButton.vue
@@ -7,7 +7,14 @@
     <label for="button-toggle" class="floating-compose-button__button"
       ><span class="floating-compose-button__button-icon">&nbsp;</span></label
     >
-    <div class="floating-compose-button__background">&nbsp;</div>
+    <div class="floating-compose-button__background">
+      <div class="icon">&#9757;</div>
+      <input
+        type="text"
+        class="form__input"
+        placeholder="Search"
+        id="search" />
+    </div>
   </div>
 </template>
 
