412650e9daba4b7df9be2178eee43aa979496fac
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-09-22 08:03:28 +0800

    Bug Fixed

--- app/src/components/sub-components/FloatingComposeButton.vue
+++ app/src/components/sub-components/FloatingComposeButton.vue
@@ -7,8 +7,13 @@
     <label for="button-toggle" class="floating-compose-button__button"
       ><span class="floating-compose-button__button-icon">&nbsp;</span></label
     >
-    <div class="floating-compose-button__background">
-      <div class="icon">&#9757;</div>
+    <div
+      class="floating-compose-button__background"
+      id="floating-compose-button__background">
+      <input type="checkbox" class="form__checkbox" id="button-toggle-2" />
+      <label for="button-toggle-2" class="form__checkbox-icon">
+        <div class="icon">&#9757;</div></label
+      >
       <input
         type="text"
         class="form__input"
