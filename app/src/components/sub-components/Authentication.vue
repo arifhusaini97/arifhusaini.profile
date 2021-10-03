0301ada2339a563e03c038c5c86fd6f38b2c8f76
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-10-03 14:09:17 +0800

    Login Content - UI for Authentication and Notes
    Changes to be committed:
    	modified:   app/src/components/sub-components/Authentication.vue
    	modified:   app/src/components/sub-components/Note.vue
    	modified:   app/src/sass/base/_base.scss
    	modified:   app/src/sass/components/_button.scss
    	modified:   app/src/sass/components/_form.scss
    	modified:   app/src/sass/components/_login.scss
    	new file:   app/src/sass/layout/padding.scss
    	modified:   app/src/sass/main.scss

--- app/src/components/sub-components/Authentication.vue
+++ app/src/components/sub-components/Authentication.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="authentication">
-    <div class="center card neumorphism">Auth</div>
+    <div class="center card neumorphism form-group">
+      <h1>Login</h1>
+      <form method="post" class="form pt-1">
+        <input
+          type="text"
+          name="u"
+          placeholder="Username"
+          required="required" />
+        <input
+          type="password"
+          name="p"
+          placeholder="Password"
+          required="required" />
+        <button type="submit" class="btn btn-primary btn-block btn-large">
+          Let me in.
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
--- app/src/components/sub-components/Note.vue
+++ app/src/components/sub-components/Note.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="note">
-    <div class="center card neumorphism">Note</div>
+    <div class="center card neumorphism form-group">
+      <h1>Note</h1>
+      <div class="form">
+        <div class="form__note">{{ note }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default { name: 'Note' };
+  export default {
+    name: 'Note',
+    data() {
+      return {
+        note: `
+        Please login to use the system.
+        Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Possimus voluptas nam quaerat
+        impedit, est enim fuga, sint dolores eaque dolor
+        labore unde veritatis modi, accusantium ut. Dolores
+        enim perspiciatis nulla. modi, accusantium ut. Dolores
+        enim perspiciatis nulla.`,
+      };
+    },
+  };
 </script>
 
 <style></style>
